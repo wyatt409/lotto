443dd4307124b014b2d1fe4bdae6fa798f861145
Kim Yu Hyun <yu1234hyun@gmail.com>
2021-12-24 15:55:30 +0700

    aa

---
+++ android/app/src/main/kotlin/com/io_developer/lotto/lotto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.io_developer.lotto.lotto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int randomNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Random apple = Random();
+
+          setState(() {
+            randomNumber = apple.nextInt(10000);
+          });
+        },
+        child: Scaffold(
+          body: Center(
+              child: Text("$randomNumber")
+          ),
+        )
+    );
+  }
+}
